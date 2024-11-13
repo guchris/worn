e7e2eca3651aa01d37529773221504d34ee90a7f
Christopher Gu <cleegu@gmail.com>
2024-11-12 23:56:12 -0800

    1) cleaned up closet page to basic text with nav bar

--- src/app/closet/page.tsx
+++ src/app/closet/page.tsx
@@ -1,12 +1,5 @@
 // App Imports
 import NavBar from "@/components/nav-bar"
-import SidebarApp from "@/components/sidebar-app"
-import ClosetGrid from "@/components/closet-grid"
-
-// Shadcn Imports
-import { Separator } from "@/components/ui/separator"
-import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbPage } from "@/components/ui/breadcrumb"
-import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar"
 
 export default function ClosetPage() {
 	return (
@@ -16,28 +9,5 @@ export default function ClosetPage() {
                 <p className="text-sm">closet</p>
             </div>
 		</div>
-		// <div>
-		// 	<SidebarProvider>
-
-		// 		<SidebarApp />
-
-		// 		<SidebarInset>
-		// 			<header className="sticky top-0 flex h-16 items-center gap-2 border-b bg-background px-4">
-		// 				<SidebarTrigger className="-ml-1" />
-		// 				<Separator orientation="vertical" className="mr-2 h-4" />
-		// 				<Breadcrumb>
-		// 					<BreadcrumbList>
-		// 						<BreadcrumbItem>
-		// 							<BreadcrumbPage>My Closet</BreadcrumbPage>
-		// 						</BreadcrumbItem>
-		// 					</BreadcrumbList>
-		// 				</Breadcrumb>
-		// 			</header>
-		// 			<div className="flex flex-1 flex-col gap-4 p-4">
-		// 				{/* <ClosetGrid /> */}
-		// 			</div>
-		// 		</SidebarInset>
-		// 	</SidebarProvider>
-		// </div>
 	)
 }
