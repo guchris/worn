b67f076cfe5dd1522a3a2363fdc1175132903183
Christopher Gu <cleegu@gmail.com>
2024-11-18 21:18:12 -0800

    1) updated closet to include closet grid for the logged-in user

--- src/app/closet/page.tsx
+++ src/app/closet/page.tsx
@@ -1,24 +1,54 @@
 "use client"
 
 // Next and React Imports
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import Link from "next/link"
 
 // App Imports
+import { Item } from "@/lib/types"
 import NavBar from "@/components/nav-bar"
 import { useAuth } from "@/context/AuthContext"
+import ClosetGrid from "@/components/closet-grid"
+
+// Firebase Imports
+import { collection, getDocs } from "firebase/firestore"
+import { db } from "@/lib/firebase"
 
 export default function ClosetPage() {
-	const { user } = useAuth();
+	const { user, loading: authLoading } = useAuth();
     const router = useRouter();
+	const [closetItems, setClosetItems] = useState<Item[]>([]);
+    const [loading, setLoading] = useState<boolean>(true);
 
-	// Redirect only when "user" changes
-	useEffect(() => {
-        if (!user) {
+	 // Redirect to login only after authLoading is complete
+	 useEffect(() => {
+        if (!authLoading && !user) {
             router.push("/auth/login");
         }
-    }, [user, router]);
+    }, [authLoading, user, router]);
+
+	// Fetch closet items for the current user
+    useEffect(() => {
+        const fetchClosetItems = async () => {
+            if (!user) return;
+
+            setLoading(true);
+
+            try {
+                const closetCollection = collection(db, `users/${user.uid}/closet`);
+                const snapshot = await getDocs(closetCollection);
+                const items = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() })) as Item[];
+                setClosetItems(items);
+            } catch (error) {
+                console.error("Error fetching closet items:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchClosetItems();
+    }, [user]);
 
 	// Show nothing while redirecting
     if (!user) {
@@ -28,13 +58,14 @@ export default function ClosetPage() {
 	return (
 		<div className="relative flex flex-col md:flex-row min-h-screen overflow-hidden">
             <NavBar />
-            <div className="w-full p-6 md:p-8">
+            <div className="w-full p-6 md:p-8 space-y-4">
 				<div className="flex items-center justify-between">
 					<p className="hidden md:block text-sm">closet</p>
 					<Link href="/add-item" className="text-sm hover:text-green-500" passHref>
 						+ add clothing item
 					</Link>
 				</div>
+				<ClosetGrid items={closetItems} loading={loading} />
             </div>
 		</div>
 	)
