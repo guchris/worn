8588f3c6b7c7a5a5ee4f49607ff1d2c2fce07697
Christopher Gu <cleegu@gmail.com>
2024-12-10 14:09:07 -0800

    1) generator cards actually to generate random items upon page launch, 2) new randomize button to generate random items for all 3 cards at once

--- src/app/generator/page.tsx
+++ src/app/generator/page.tsx
@@ -15,7 +15,8 @@ import { collection, getDocs } from "firebase/firestore"
 import { db } from "@/lib/firebase"
 
 // Shadcn Imports
-import { Card, CardHeader, CardContent } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent } from "@/components/ui/card"
 
 export default function GeneratorPage() {
     
@@ -79,9 +80,9 @@ export default function GeneratorPage() {
 
                 // Generate initial random options
                 setSelectedOptions({
-                    accessories: accessories.length > 0 ? accessories[0].images[0] : "",
-                    tops: tops.length > 0 ? tops[0].images[0] : "",
-                    bottoms: bottoms.length > 0 ? bottoms[0].images[0] : "",
+                    accessories: accessories.length > 0 ? accessories[Math.floor(Math.random() * accessories.length)].images[0] : "",
+                    tops: tops.length > 0 ? tops[Math.floor(Math.random() * tops.length)].images[0] : "",
+                    bottoms: bottoms.length > 0 ? bottoms[Math.floor(Math.random() * bottoms.length)].images[0] : "",
                 });
             } catch (error) {
                 console.error("Error fetching closet items:", error);
@@ -110,7 +111,23 @@ export default function GeneratorPage() {
             }));
         }
     };
-    
+
+    const randomizeAll = () => {
+        setSelectedOptions({
+            accessories:
+                closetItems.accessories.length > 0
+                    ? closetItems.accessories[Math.floor(Math.random() * closetItems.accessories.length)].images[0]
+                    : "",
+            tops:
+                closetItems.tops.length > 0
+                    ? closetItems.tops[Math.floor(Math.random() * closetItems.tops.length)].images[0]
+                    : "",
+            bottoms:
+                closetItems.bottoms.length > 0
+                    ? closetItems.bottoms[Math.floor(Math.random() * closetItems.bottoms.length)].images[0]
+                    : "",
+        });
+    };
 
     if (loading) {
         return null;
@@ -123,22 +140,23 @@ export default function GeneratorPage() {
 	return (
 		<div className="relative flex flex-col md:flex-row min-h-screen overflow-hidden">
             <NavBar />
-            <main className="flex-1 px-6 pb-6 space-y-6 md:p-8">
+            <main className="flex-1 px-6 pb-6 space-y-4 md:p-8">
+                <Button variant="secondary" onClick={randomizeAll}>randomize</Button>
                 <div className="space-y-2">
                     {["accessories", "tops", "bottoms"].map((category) => (
                         <Card
                             key={category}
-                            className="shadow-none rounded-md cursor-pointer md:w-[300px]"
+                            className="shadow-none rounded-md cursor-pointer md:w-[200px]"
                             onClick={() => generateRandomOption(category)}
                         >
                             <CardContent className="p-0 flex justify-center items-center">
                                 {selectedOptions[category as keyof typeof selectedOptions] && (
                                     <Image
-                                    src={selectedOptions[category as keyof typeof selectedOptions]}
-                                    alt={`${category} image`}
-                                    width={300}
-                                    height={400}
-                                    className="object-cover"
+                                        src={selectedOptions[category as keyof typeof selectedOptions]}
+                                        alt={`${category} image`}
+                                        width={150}
+                                        height={200}
+                                        className="object-cover"
                                     />
                                 )}
                             </CardContent>
